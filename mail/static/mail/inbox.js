b7cbfd34f8b1546d3b0da36b7ea807fc74484271
Ezemol <ezemolina06@gmail.com>
2024-08-24 20:21:45 -0300

    get_email by id function completed, started send_email function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -23,6 +23,10 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // TODO logica de envío
+  send_email()
+
 }
 
 function load_mailbox(mailbox) {
@@ -42,6 +46,7 @@ function load_mailbox(mailbox) {
 // conseguir emails en una página
 // Mandar GET y recibir emails en X mailbox
 function get_emails(mailbox) {
+
   fetch(`/emails/${mailbox}`) 
   .then(response => response.json())
   .then(emails => {
@@ -53,6 +58,7 @@ function get_emails(mailbox) {
       // Crear div para cada email
       const newDiv = document.createElement('div');
       newDiv.className = 'email-item';
+      newDiv.id = `email-item-${email.id}`;
 
       // Agregar cada email nuevo
       newDiv.innerHTML = `
@@ -60,15 +66,62 @@ function get_emails(mailbox) {
       <p><strong>To:</strong> ${email.recipients.join(', ')}</p>   
       <p><strong>Subject:</strong> ${email.subject}</p> 
       <p><strong>Body:</strong> ${email.body}</p> 
-      <p><strong>Date:</strong> ${email.timestamp}</p> 
+      <p><strong>Date:</strong> ${email.timestamp}</p>
+      <p><button id='email-button-${email.id}'>Go to Email</button></p>
       `; // .join(", ") sirve por si hay varios destinatarios, que aparezcan separados por una coma
 
+      // Agregar div con email
       document.querySelector('#emails-view').appendChild(newDiv);
     });
+
+    // Correr función para seleccionar un email con id específico.
+    document.querySelector(`#email-button-${email.id}`).addEventListener('click', () => get_email(`${email.id}`));
   })
   .catch(error => {
     // Manejo de errores
     console.error('Invalid mailbox.', error);
   }); 
 }
-});
+
+
+// GET email por su id
+function get_email(email_id) {
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    // Imprimir en consola
+    console.log(email);
+
+    // Mostrar este email en pantalla
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector(`#email-item-${email_id}`).style.display = 'block';
+
+  })
+  .catch(error => {
+    // Manejo de errores
+    console.error('Email not found', error);
+  });
+}
+
+// Function para mandar email
+// TODO
+function send_email(recipients, subject, body) {
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients, // receiber of post
+      subject: subject, // Subject of post
+      body: body // Body of post
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    console.log(result);
+  })
+  .catch(error => {
+    console.error('At least one recipient required.', error)
+  });
+}
+
+}); // Fin de todo
